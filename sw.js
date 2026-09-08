38e2f7954a25588063be285e719538a5d42def38
xylotokq <mylokq@gmail.com>
2026-09-08 10:14:17 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="kyle-fit-v7";
+const CACHE="kyle-fit-v7-1";
 const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-180.png","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",event=>{
   event.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
