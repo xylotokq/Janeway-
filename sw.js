79cb973f42d1eaf1a3872bb3ac1d739a8f0ff5df
xylotokq <mylokq@gmail.com>
2026-09-09 16:47:53 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,10 +1,12 @@
-const CACHE="kyle-fit-v9-9-2-full";
-const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-180.png","./icon-192.png","./icon-512.png","./xiaochou.jpg?v=992"];
+const CACHE="kyle-fit-v9-9-3-full";
+const ASSETS=["./","./index.html","./manifest.webmanifest","./icon-180.png","./icon-192.png","./icon-512.png","./xiaochou.jpg?v=993"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{
  if(e.request.method!=="GET")return;
  e.respondWith(fetch(e.request,{cache:"no-store"}).then(r=>{
-  const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));return r;
+  const c=r.clone();
+  caches.open(CACHE).then(x=>x.put(e.request,c));
+  return r;
  }).catch(()=>caches.match(e.request).then(r=>r||caches.match("./index.html"))))
 });
